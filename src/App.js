6bb2d288801d8dc3674c785050d2580285634f5e
MaseedZafar <zafarmaseed1@gmail.com>
2022-05-28 14:32:51 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecom Website</h1>
+      <Button>Normal Button</Button>
+      <button>Boostart Button</button>
+    </div>
+  );
+}
+
+export default App;
